cd63a09dbbb9c4a947b3b7977d8c953d8803bdd4
sharmeen-123 <fa20-bcs-088@isbstudent.comsats.edu.pk>
2024-06-14 16:45:27 +0500

    changes: navbar

--- src/components/NavbarMain.js
+++ src/components/NavbarMain.js
@@ -27,15 +27,21 @@ const NavbarMain = () => {
   
   <ul className="flex font-medium p-4 md:p-0 sm:p-0 ml-4 gap-6 md:gap-1 sm:gap-1 font-league-spartan text-base items-center  flex-row md:space-x-1 sm:space-x-1 md:mt-0 md:border-0">
       <li >
-        <a href="#about-us" className= 'block py-3 pl-4 pr-4 sm:py-1 sm:px-2 md:py-2 md:px-2 text-white text-base sm:text-[8px] hover:bg-white hover:text-[#1B1C1C] active:text-[#1B1C1C] active:bg-white rounded-[16px] '>About us</a>
+      <a href="#about-us" class="block py-3 pl-4 pr-4 sm:py-1 sm:px-0.5 md:py-2 md:px-2 text-white text-base sm:text-[8px] hover:bg-white hover:text-[#1B1C1C] active:text-[#1B1C1C] active:bg-white rounded-[16px] flex items-center">
+    <span class="whitespace-nowrap">About us</span>
+</a>
+  </li>
+      <li >
+        <a href="#features" className= 'block py-3 pl-4 pr-4 sm:py-1 sm:px-0.5 md:py-2 md:px-2 text-white text-base sm:text-[8px] hover:bg-white hover:text-[#1B1C1C] active:text-[#1B1C1C] active:bg-white rounded-[16px] '>Features</a>
       </li>
       <li >
-        <a href="#features" className= 'block py-3 pl-4 pr-4 sm:py-1 sm:px-2 md:py-2 md:px-2 text-white text-base sm:text-[8px] hover:bg-white hover:text-[#1B1C1C] active:text-[#1B1C1C] active:bg-white rounded-[16px] '>Features</a>
+        <a href="#contact" className= 'block py-3 pl-4 pr-4 sm:py-1 sm:px-0.5 md:py-2 md:px-2 text-white text-base sm:text-[8px] hover:bg-white hover:text-[#1B1C1C] active:text-[#1B1C1C] active:bg-white rounded-[16px] '>
+        <span class="whitespace-nowrap">Contact us </span>
+        </a>
       </li>
-      <li >
-        <a href="#contact" className= 'block py-3 pl-4 pr-4 sm:py-1 sm:px-2 md:py-2 md:px-2 text-white text-base sm:text-[8px] hover:bg-white hover:text-[#1B1C1C] active:text-[#1B1C1C] active:bg-white rounded-[16px] '>Contact us</a>
-      </li>
-      <a data-icon="Contactus" href="#Contactus" className= 'ml-16 sm:h-fit sm:ml-0 block py-3 pl-6 pr-6 sm:py-1 sm:px-2 md:py-2 md:px-2 text-[#000] font-bold text-base md:text-xs sm:text-[8px]  bg-[#FFF528] rounded-lg sm:rounded-md '>Sign Up</a>
+      <a data-icon="Contactus" href="#Contactus" className= 'ml-16 sm:h-fit sm:ml-0 block py-3 pl-6 pr-6 sm:py-1 sm:px-0.5 md:py-2 md:px-2 text-[#000] font-bold text-base md:text-xs sm:text-[8px]  bg-[#FFF528] rounded-lg sm:rounded-md '>
+      <span class="whitespace-nowrap">Sign Up</span>
+      </a>
  
       </ul>
  
