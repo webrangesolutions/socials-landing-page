9a77185813c42c2d352b5f67bc3e30a918ddc4c7
sharmeen-123 <fa20-bcs-088@isbstudent.comsats.edu.pk>
2024-07-11 13:02:46 +0500

    landing page

--- src/components/emailForm.jsx
+++ src/components/emailForm.jsx
@@ -1,31 +1,6 @@
 import React, { useEffect } from 'react';
 
 const EmailForm = ({ onClose }) => {
-  // useEffect(() => {
-  //   // Load AWeber form script
-  //   const script = document.createElement('script');
-  //   script.src = "//forms.aweber.com/form/55/907931455.js";
-  //   script.id = "aweber-wjs-g7ve8s4fn";
-  //   document.body.appendChild(script);
-
-  //   // Handler for form submission
-  //   const handleSubmit = (event) => {
-  //     if (event.target.tagName === 'FORM') {
-  //       onClose()
-  //       // Add your form submission logic here
-  //     }
-  //   };
-
-  //   // Add submit event listener to the document body
-  //   document.body.addEventListener('submit', handleSubmit);
-
-  //   // Cleanup script and event listener when component unmounts
-  //   return () => {
-  //     document.body.removeChild(script);
-  //     document.body.removeEventListener('submit', handleSubmit);
-  //   };
-  // }, []);
-
   useEffect(() => {
     // Load AWeber form script
     const script = document.createElement('script');
@@ -33,11 +8,44 @@ const EmailForm = ({ onClose }) => {
     script.id = "aweber-wjs-g7ve8s4fn";
     document.body.appendChild(script);
 
-    // Cleanup the script when component unmounts
+    // Handler for form submission
+    const handleSubmit = (event) => {
+      if (event.target.tagName === 'FORM') {
+        event.preventDefault(); // Prevent default form submission behavior
+        
+        // Get the form action URL
+        const actionUrl = event.target.action;
+
+        // Open the action URL in a new window
+        window.open(actionUrl, '_blank');
+
+        // Close the form
+        onClose();
+      }
+    };
+
+    // Add submit event listener to the document body
+    document.body.addEventListener('submit', handleSubmit);
+
+    // Cleanup script and event listener when component unmounts
     return () => {
       document.body.removeChild(script);
+      document.body.removeEventListener('submit', handleSubmit);
     };
-  }, []);
+  }, [onClose]);
+
+  // useEffect(() => {
+  //   // Load AWeber form script
+  //   const script = document.createElement('script');
+  //   script.src = "//forms.aweber.com/form/55/907931455.js";
+  //   script.id = "aweber-wjs-snz94ybot";
+  //   document.body.appendChild(script);
+
+  //   // Cleanup the script when component unmounts
+  //   return () => {
+  //     document.body.removeChild(script);
+  //   };
+  // }, []);
 
   return (
     <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-gray-800 bg-opacity-50 z-50">
